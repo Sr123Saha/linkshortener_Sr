51eb12335fc89903b1fb405c1e596a28c75a0883
Sr123Saha <150663954+Sr123Saha@users.noreply.github.com>
2023-11-28 15:47:52 +0300

    nothing
    nothing"zxc"

--- finished_project.py
+++ finished_project.py
@@ -1,14 +1,11 @@
-import keyboard
 import pyperclip
 import pyshorteners
 from tkinter import ttk
 from tkinter import messagebox
 from tkinter import *
 
-
-
+#213121231231313131231213213
 def shorten_url():
-    
     original_url = window_to_insert_a_link.get()
     s = pyshorteners.Shortener()
     shortened_url = s.tinyurl.short(original_url)
@@ -58,17 +55,4 @@ label_result.place(x=36, y=110)
 window_to_insert_a_link_two = ttk.Entry(window, width=40, font="Arial 27")
 window_to_insert_a_link_two.place(x=40, y=135)
 
-# while True:
-#     if not(keyboard.on_pressed('q')):
-#         break
-#     elif keyboard.is_pressed('esc'):
-#         print('esc')
-#         quit()
-
-while True:
-    # Wait for the next event.
-    event = keyboard.read_event()
-    if event.event_type == keyboard.KEY_DOWN and event.name  == 'space':
-        print('space was pressed')
-        window.mainloop()
-
+window.mainloop()
