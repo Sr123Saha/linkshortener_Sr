f29c65c518cc70b415e5cf03be7439c21c9288fd
Sr123Saha <150663954+Sr123Saha@users.noreply.github.com>
2023-11-28 15:37:38 +0300

    I've edited the interface and added "requirements"
    I've edited the interface and added requirements

--- finished_project.py
+++ finished_project.py
@@ -1,48 +1,74 @@
-# Сюда закидываем чистый код без описания и коментариев
-#("16.11.2023.")
-#("17.11.2023")
-#("19.11.2023")# редоктирование под главвное меню
+import keyboard
 import pyperclip
 import pyshorteners
 from tkinter import ttk
 from tkinter import messagebox
 from tkinter import *
-def delete_all(self):
-    self.text.delete(0, 'end')
+
+
+
 def shorten_url():
+    
     original_url = window_to_insert_a_link.get()
-
     s = pyshorteners.Shortener()
-    shortened_url = s.tinyurl.short(original_url) 
-
+    shortened_url = s.tinyurl.short(original_url)
     window_to_insert_a_link_two.delete(0, 'end')
     window_to_insert_a_link_two.insert(0, shortened_url)
+    pyperclip.copy(shortened_url)
+    messagebox.showinfo("СОКРАЩАТЕЛЬ ССЫЛОЕК", "ВАША ССЫЛКА В УКОРОЧЕННОМ ФОРМАТЕ СКОПИРОВАНА В БУФЕР ОБМЕНЯ")
 
 
-window =  Tk()
-window.title('Сокращятель ссылок')
+def clear_entry():
+    window_to_insert_a_link.delete(0, 'end')
+    window_to_insert_a_link_two.delete(0, 'end')
+
+
+
+window = Tk()
+window.title('Сокращатель ссылок')
 window.geometry('1000x250')
 window["bg"] = "#68FCF5"
-window.resizable(width=False , height=False)
-1
-button = Label(window, text = "BВЕДИТЕ ССЫЛКУ" ,font=("Arial Bold", 18))
-button["bg"] = "#68FCF5"
-button.place(x=36, y=10)
+window.resizable(width=False, height=False)
 
-window_to_insert_a_link = ttk.Entry(window , width=40, font="Arial 27")# это размер окошка для ссылки
-window_to_insert_a_link.place(x = 40 , y = 35) 
 
-button_to_execute_the_code = ttk.Button(window, text="                  СОКРАТИТЬ                   ", command=shorten_url, style="TButton")
+label_instruction = Label(window, text="Введите ссылку:", font=("Arial Bold", 18), bg="#68FCF5")
+label_instruction.place(x=36, y=10)
+
+
+window_to_insert_a_link = ttk.Entry(window, width=40, font="Arial 27")
+window_to_insert_a_link.place(x=40, y=35)
+
+
+button_to_execute_the_code = ttk.Button(window, text="       СОКРАТИТЬ        ", command=shorten_url, style="TButton")
 button_to_execute_the_code.place(relx=0.45, y=99, anchor=CENTER)
+
+
+button_to_clear_entries = ttk.Button(window, text="        СТЕРЕТЬ ВСЕ      ", command=clear_entry)
+button_to_clear_entries.place(relx=0.45, y=200, anchor=CENTER)
+
+
 s = ttk.Style()
-s.configure("TButton", foreground="blue" , background="black" )
+s.configure("TButton", foreground="blue", background="black")
 
 
-button_two = Label(window ,text = "РЕЗУЛЬТАТ" ,font=("Arial Bold", 18))
-button_two["bg"] = "#68FCF5"
-button_two.place(x=36, y=110)
-window_to_insert_a_link_two = ttk.Entry(window , width=40, font="Arial 27")# это размер окошка для ссылки
-window_to_insert_a_link_two.place(x = 40 , y = 135) 
+label_result = Label(window, text="Результат:", font=("Arial Bold", 18), bg="#68FCF5")
+label_result.place(x=36, y=110)
 
 
-window.mainloop()
+window_to_insert_a_link_two = ttk.Entry(window, width=40, font="Arial 27")
+window_to_insert_a_link_two.place(x=40, y=135)
+
+# while True:
+#     if not(keyboard.on_pressed('q')):
+#         break
+#     elif keyboard.is_pressed('esc'):
+#         print('esc')
+#         quit()
+
+while True:
+    # Wait for the next event.
+    event = keyboard.read_event()
+    if event.event_type == keyboard.KEY_DOWN and event.name  == 'space':
+        print('space was pressed')
+        window.mainloop()
+
