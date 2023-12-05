7cb3f1f99f417398c4abf4d4ba2e55a8dad03e6b
Sr123Saha <150663954+Sr123Saha@users.noreply.github.com>
2023-12-05 19:34:44 +0300

    added the ability to insert a link with a Russian keyboard layout
    added the ability to insert a link with a Russian keyboard layout.
    changed the colors

--- finished_project.py
+++ finished_project.py
@@ -1,65 +1,65 @@
-
 import pyperclip
 import pyshorteners
 from tkinter import ttk
 from tkinter import messagebox
 from tkinter import *
+
+
 class Application:
     def __init__(self):
-        window = Tk()
-        window.title('ПРОГРАММА ДЛЯ СОКРАЩЕНИЯЙ ССЫЛОК')
-        window.geometry('1000x250')
-        window["bg"] = "#91908D"
-        window.resizable(width=False, height=False)
-def shorten_url():
-    original_url = window_to_insert_a_link.get()
-    s = pyshorteners.Shortener()
-    shortened_url = s.tinyurl.short(original_url)
-    window_to_insert_a_link_two.delete(0, 'end')
-    window_to_insert_a_link_two.insert(0, shortened_url)
-    pyperclip.copy(shortened_url)
-    messagebox.showinfo("ПРОГРАММА ДЛЯ СОКРАЩЕНИЯЙ ССЫЛОК", "ВАША ССЫЛКА В УКОРОЧЕННОМ ФОРМАТЕ СКОПИРОВАНА В БУФЕР ОБМЕНЯ")
+        self.window = Tk()
+        self.window.title('ПРОГРАММА ДЛЯ СОКРАЩЕНИЯЙ ССЫЛОК')
+        self.window.geometry('1000x250')
+        self.window["bg"] = "#91908D"
+        self.window.resizable(width=False, height=False)
 
-def clear_entry():
-    window_to_insert_a_link.delete(0, 'end')
-    window_to_insert_a_link_two.delete(0, 'end')
+        label_instruction = Label(self.window, text="Введите ссылку:", font=("Arial Bold", 18), bg="#91908D")
+        label_instruction.place(x=36, y=5)
 
-window = Tk()
-window.title('ПРОГРАММА ДЛЯ СОКРАЩЕНИЯЙ ССЫЛОК')
-window.geometry('1000x250')
-window["bg"] = "#91908D"
-window.resizable(width=False, height=False)
+        self.window_to_insert_a_link = ttk.Entry(self.window, width=40, font="Arial 27",)
+        self.window_to_insert_a_link.place(x=40, y=35)
 
-label_instruction = Label(window, text="Введите ссылку:", font=("Arial Bold", 18), bg="#91908D")
-label_instruction.place(x=36, y=5)
+        button_to_execute_the_code = ttk.Button(self.window, text="       СОКРАТИТЬ        ", command=self.shorten_url, style="TButton")
+        button_to_execute_the_code.place(relx=0.45, y=99, anchor=CENTER)
 
-window_to_insert_a_link = ttk.Entry(window, width=40, font="Arial 27",)
-window_to_insert_a_link.place(x=40, y=35)
+        button_to_clear_entries = ttk.Button(self.window, text="        СТЕРЕТЬ ВСЕ      ", command=self.clear_entry)
+        button_to_clear_entries.place(relx=0.45, y=200, anchor=CENTER)
 
-button_to_execute_the_code = ttk.Button(window, text="       СОКРАТИТЬ        ", command=shorten_url, style="TButton")
-button_to_execute_the_code.place(relx=0.45, y=99, anchor=CENTER)
+        s = ttk.Style()
+        s.configure("TButton", foreground="#000000", background="black")
 
-button_to_clear_entries = ttk.Button(window, text="        СТЕРЕТЬ ВСЕ      ", command=clear_entry)
-button_to_clear_entries.place(relx=0.45, y=200, anchor=CENTER)
+        label_result = Label(self.window, text="Результат:", font=("Arial Bold", 18), bg="#91908D")
+        label_result.place(x=36, y=105)
 
-s = ttk.Style()
-s.configure("TButton", foreground="#000000", background="black")
+        self.window_to_insert_a_link_two = ttk.Entry(self.window, width=40, font="Arial 27")
+        self.window_to_insert_a_link_two.place(x=40, y=135)
 
-label_result = Label(window, text="Результат:", font=("Arial Bold", 18), bg="#91908D")
-label_result.place(x=36, y=105)
+        self.window.bind_all('<Key>', self.keypress)  # Bind the keypress function to all keys
+        self.window.mainloop()
 
-window_to_insert_a_link_two = ttk.Entry(window, width=40, font="Arial 27")
-window_to_insert_a_link_two.place(x=40, y=135)
+    def shorten_url(self):
+        original_url = self.window_to_insert_a_link.get()
+        
+        s = pyshorteners.Shortener()
+        shortened_url = s.tinyurl.short(original_url)
+        
+        self.window_to_insert_a_link_two.delete(0, 'end')
+        self.window_to_insert_a_link_two.insert(0, shortened_url)
+        
+        pyperclip.copy(shortened_url)
+        messagebox.showinfo("ПРОГРАММА ДЛЯ СОКРАЩЕНИЯЙ ССЫЛОК", "ВАША ССЫЛКА В УКОРОЧЕННОМ ФОРМАТЕ СКОПИРОВАНА В БУФЕР ОБМЕНЯ")
 
-window.mainloop()
-def keypress(event):
-    if event.keycode == 86:
-        event.widget.event_generate('<<Paste>>')
-    elif event.keycode == 67:
-        event.widget.event_generate('<<Copy>>')
-    elif event.keycode == 88:
-        event.widget.event_generate('<<Cut>>')
+    def clear_entry(self):
+        self.window_to_insert_a_link.delete(0, 'end')
+        self.window_to_insert_a_link_two.delete(0, 'end')
 
+    def keypress(self, event):
+        if event.keycode == 86:
+            event.widget.event_generate('<<Paste>>')
+        elif event.keycode == 67:
+            event.widget.event_generate('<<Copy>>')
+        elif event.keycode == 88:
+            event.widget.event_generate('<<Cut>>')
 
 if __name__ == '__main__':
     app = Application()
